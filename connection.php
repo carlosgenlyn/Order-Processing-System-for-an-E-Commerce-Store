f73c5c7d2e4178ad36ba054bacda2376d0ac5cf8
Jos6996 <joshuagaspar477@gmail.com>
2025-04-23 19:15:06 +0800

    tasks 1, 2 and 3

---
+++ connection.php
@@ -0,0 +1,15 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // your MySQL password if applicable
+$database = "db_Carlos_project";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+session_start();
+?>
--- schema.sql
+++ schema.sql
@@ -1,14 +1,15 @@
- create database db_batch8_capstone;
 
- use db_batch8_capstone;
+CREATE DATABASE db_Carlos_project;
 
- -- Table: Users
- CREATE TABLE Users (
+USE db_Carlos_project;
+
+CREATE TABLE Users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     phone VARCHAR(15),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    ALTER TABLE Users ADD role ENUM('user', 'admin') DEFAULT 'user';
+
 );
- )
